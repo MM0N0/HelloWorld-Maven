4ab22708f63c23a7540661921711148e7d91f406
MM0N0 <116948996+MM0N0@users.noreply.github.com>
2022-10-29 19:36:06 +0200

    initial commit

---
+++ src/main/java/testpackage/test/App.java
@@ -0,0 +1,12 @@
+package testpackage.test;
+
+import org.tinylog.Logger;
+
+public class App
+{
+
+    public static void main( String[] args )
+    {
+        Logger.info( "Hello World!" );
+    }
+}
